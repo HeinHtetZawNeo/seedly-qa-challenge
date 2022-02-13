c5fedce168c0c93a7f89d6fa599521c1dfaaae1f
Hein Htet Zaw <hein@perxtech.com>
2022-02-13 17:53:05 +0800

    add automation testcases

--- cypress/data/page/reviewPage.js
+++ cypress/data/page/reviewPage.js
@@ -6,6 +6,14 @@ module.exports = {
     },
     productitem_div: {
       selector: "//div[@data-testid='product-item']",
+      testid: "product-item",
+    },
+    noofproductfound_p: {
+      selector: "//*[@id='__next']/div/div[2]/div[3]/div/div[2]/div[1]/p",
+    },
+    noproductfound_div: {
+      selector: "//*[@id='__next']/div/div[2]/div[3]/div/div[2]/div[3]/div[1]",
+      value: "No products found!",
     },
   },
 };
--- cypress/integration/review_page.spec.js
+++ cypress/integration/review_page.spec.js
@@ -2,7 +2,7 @@
 
 let ReviewPage = require("../data/page/reviewPage");
 
-context("Review Page Testing", () => {
+context("Dasktop WebView Testing", () => {
   beforeEach(() => {
     cy.viewport(1280, 720);
   });
@@ -20,7 +20,6 @@ context("Review Page Testing", () => {
     //wait review page to finsih loading (wait with campaigns api)
     cy.wait("@getCampaigns");
 
-    //cy.intercept("https://api.seedly.sg/api/v4/product/items/*").as("getItems");
     cy.intercept("GET", "https://api.seedly.sg//api/v4/product/items*").as(
       "getItems"
     );
@@ -33,19 +32,21 @@ context("Review Page Testing", () => {
     cy.wait("@getItems");
 
     //get product item
-    cy.get('[data-testid="product-item"]').within((productItem) => {
-      //Singtel Dash PET should exist
-      cy.get("a").should("be.visible").should("contain", "Singtel Dash PET");
+    cy.xpath(ReviewPage.page_items.productitem_div.selector).within(
+      (productItem) => {
+        //Singtel Dash PET should exist
+        cy.get("a").should("be.visible").should("contain", "Singtel Dash PET");
 
-      //view more button should exist
-      cy.get("a").should("be.visible").should("contain", "View More");
+        //view more button should exist
+        cy.get("a").should("be.visible").should("contain", "View More");
 
-      //remove target attribute not to create new tab
-      cy.get("a").eq(1).invoke("removeAttr", "target").click();
+        //remove target attribute not to create new tab
+        cy.get("a").eq(1).invoke("removeAttr", "target").click();
 
-      //check url is should be singtel-dash-pet
-      cy.url().should("include", "/singtel-dash-pet");
-    });
+        //check url is should be singtel-dash-pet
+        cy.url().should("include", "/singtel-dash-pet");
+      }
+    );
   });
 
   it("tcs-2: Search Existing Product with partial name", () => {
@@ -59,7 +60,6 @@ context("Review Page Testing", () => {
     //wait review page to finsih loading (wait with campaigns api)
     cy.wait("@getCampaigns");
 
-    //cy.intercept("https://api.seedly.sg/api/v4/product/items/*").as("getItems");
     cy.intercept("GET", "https://api.seedly.sg//api/v4/product/items*").as(
       "getItems"
     );
@@ -70,18 +70,164 @@ context("Review Page Testing", () => {
     cy.wait("@getItems");
 
     //get product item
-    cy.get('[data-testid="product-item"]').within((productItem) => {
-      //Singtel Dash PET should exist
-      cy.get("a").should("be.visible").should("contain", "Singtel Dash PET");
+    cy.xpath(ReviewPage.page_items.productitem_div.selector).within(
+      (productItem) => {
+        //Singtel Dash PET should exist
+        cy.get("a").should("be.visible").should("contain", "Singtel Dash PET");
 
-      //view more button should exist
-      cy.get("a").should("be.visible").should("contain", "View More");
+        //view more button should exist
+        cy.get("a").should("be.visible").should("contain", "View More");
 
-      //remove target attribute not to create new tab
-      cy.get("a").eq(1).invoke("removeAttr", "target").click();
+        //remove target attribute not to create new tab
+        cy.get("a").eq(1).invoke("removeAttr", "target").click();
 
-      //check url is should be singtel-dash-pet
-      cy.url().should("include", "/singtel-dash-pet");
-    });
+        //check url is should be singtel-dash-pet
+        cy.url().should("include", "/singtel-dash-pet");
+      }
+    );
+  });
+
+  it("tcs-3: Search Existing Product with partial name with white spaces end of the string", () => {
+    cy.intercept("GET", "https://api.seedly.sg/api/v4/navigation/campaigns").as(
+      "getCampaigns"
+    );
+
+    //Go to review page
+    cy.visit("reviews");
+
+    //wait review page to finsih loading (wait with campaigns api)
+    cy.wait("@getCampaigns");
+
+    cy.intercept("GET", "https://api.seedly.sg//api/v4/product/items*").as(
+      "getItems"
+    );
+
+    //type Singtel Dash PET in the search box
+    cy.xpath(ReviewPage.page_items.serachbox_textbox.selector).type(
+      "Singtel                "
+    );
+
+    cy.wait("@getItems");
+
+    //get product item
+    cy.xpath(ReviewPage.page_items.productitem_div.selector).within(
+      (productItem) => {
+        //Singtel Dash PET should exist
+        cy.get("a").should("be.visible").should("contain", "Singtel Dash PET");
+
+        //view more button should exist
+        cy.get("a").should("be.visible").should("contain", "View More");
+
+        //remove target attribute not to create new tab
+        cy.get("a").eq(1).invoke("removeAttr", "target").click();
+
+        //check url is should be singtel-dash-pet
+        cy.url().should("include", "/singtel-dash-pet");
+      }
+    );
+  });
+
+  it("tcs-4: Search Existing Product with partial name with white spaces start of the string", () => {
+    cy.intercept("GET", "https://api.seedly.sg/api/v4/navigation/campaigns").as(
+      "getCampaigns"
+    );
+
+    //Go to review page
+    cy.visit("reviews");
+
+    //wait review page to finsih loading (wait with campaigns api)
+    cy.wait("@getCampaigns");
+
+    cy.intercept("GET", "https://api.seedly.sg//api/v4/product/items*").as(
+      "getItems"
+    );
+
+    //type Singtel Dash PET in the search box
+    cy.xpath(ReviewPage.page_items.serachbox_textbox.selector).type(
+      "                Singtel"
+    );
+
+    cy.wait("@getItems");
+
+    //get product item
+    cy.xpath(ReviewPage.page_items.productitem_div.selector).within(
+      (productItem) => {
+        //Singtel Dash PET should exist
+        cy.get("a").should("be.visible").should("contain", "Singtel Dash PET");
+
+        //view more button should exist
+        cy.get("a").should("be.visible").should("contain", "View More");
+
+        //remove target attribute not to create new tab
+        cy.get("a").eq(1).invoke("removeAttr", "target").click();
+
+        //check url is should be singtel-dash-pet
+        cy.url().should("include", "/singtel-dash-pet");
+      }
+    );
+  });
+
+  it("tcs-5: Search Non-existing Product with normal string", () => {
+    cy.intercept("GET", "https://api.seedly.sg/api/v4/navigation/campaigns").as(
+      "getCampaigns"
+    );
+
+    //Go to review page
+    cy.visit("reviews");
+
+    //wait review page to finsih loading (wait with campaigns api)
+    cy.wait("@getCampaigns");
+
+    cy.intercept("GET", "https://api.seedly.sg//api/v4/product/items*").as(
+      "getItems"
+    );
+
+    //type Singtel Dash PET in the search box
+    cy.xpath(ReviewPage.page_items.serachbox_textbox.selector).type("Testing");
+
+    cy.wait("@getItems");
+
+    //No Products found should show
+    cy.xpath(ReviewPage.page_items.noproductfound_div.selector)
+      .should("be.visible")
+      .should("contain", ReviewPage.page_items.noproductfound_div.value);
+
+    //No of product found should 0
+    cy.xpath(ReviewPage.page_items.noofproductfound_p.selector)
+      .should("be.visible")
+      .should("contain", "0 Products Found");
+  });
+
+  it("tcs-6: Search Non-existing Product with special char string", () => {
+    cy.intercept("GET", "https://api.seedly.sg/api/v4/navigation/campaigns").as(
+      "getCampaigns"
+    );
+
+    //Go to review page
+    cy.visit("reviews");
+
+    //wait review page to finsih loading (wait with campaigns api)
+    cy.wait("@getCampaigns");
+
+    cy.intercept("GET", "https://api.seedly.sg//api/v4/product/items*").as(
+      "getItems"
+    );
+
+    //type Singtel Dash PET in the search box
+    cy.xpath(ReviewPage.page_items.serachbox_textbox.selector).type(
+      "@$%*(%^($#&*$"
+    );
+
+    cy.wait("@getItems");
+
+    //No Products found should show
+    cy.xpath(ReviewPage.page_items.noproductfound_div.selector)
+      .should("be.visible")
+      .should("contain", ReviewPage.page_items.noproductfound_div.value);
+
+    //No of product found should 0
+    cy.xpath(ReviewPage.page_items.noofproductfound_p.selector)
+      .should("be.visible")
+      .should("contain", "0 Products Found");
   });
 });
