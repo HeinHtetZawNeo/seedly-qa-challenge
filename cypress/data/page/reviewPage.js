7ef5705b27ad771a7b1e73795275c2413b796d1b
Hein Htet Zaw <hein@perxtech.com>
2022-02-13 16:38:37 +0800

    add Review Page

---
+++ cypress/data/page/reviewPage.js
@@ -0,0 +1,11 @@
+module.exports = {
+  page_items: {
+    serachbox_textbox: {
+      selector: "/html/body/div[1]/div/div[2]/div[1]/div/div[3]/div/input",
+      value: "Find All Product",
+    },
+    productitem_div: {
+      selector: "//div[@data-testid='product-item']",
+    },
+  },
+};
