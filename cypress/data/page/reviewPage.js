f6ae8ce18cf2b57bda4e3bcae1c2954ee6a9a5a9
Hein Htet Zaw <hein@perxtech.com>
2022-02-13 22:48:02 +0800

    add page title and menu button

--- cypress/data/page/reviewPage.js
+++ cypress/data/page/reviewPage.js
@@ -1,5 +1,12 @@
 module.exports = {
   page_items: {
+    pagetitle_h1: {
+      selector: "//h1[@data-testid='review-page-title']",
+      testid: "review-page-title",
+    },
+    menu_button: {
+      selector: '//*[@id="__next"]/div/div[1]/div[1]/div/div[1]/div/button',
+    },
     serachbox_textbox: {
       selector: "/html/body/div[1]/div/div[2]/div[1]/div/div[3]/div/input",
       value: "Find All Product",
